21e537c5914d26759c0b18ee02b9131175fcab30
orlandoqm <orlandoquirogam@gmail.com>
2023-08-07 22:52:32 -0600

    pwa-comentado

--- HidroPlan/settings.py
+++ HidroPlan/settings.py
@@ -180,4 +180,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'orlandoquirogam@gmail.com'  # Coloca aquí tu dirección de correo de Gmail
 EMAIL_HOST_PASSWORD = 'tzcabjgbgcsphafo'    # Coloca aquí la contraseña de tu cuenta de Gmail
 
-#PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR,'serviceworker.js')
+##PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR,'serviceworker.js')
