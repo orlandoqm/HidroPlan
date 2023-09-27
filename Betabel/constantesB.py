8bff29b5176e27e943c0b1a2d97bcb79651492ea
orlandoqm <orlandoquirogam@gmail.com>
2023-09-27 13:32:14 -0600

    datos-del-correo-completado2

--- Betabel/constantesB.py
+++ Betabel/constantesB.py
@@ -3,9 +3,9 @@ asuntoTrasplante="¡Trasplante de betabel!"
 asuntoCosecha="¡Cosecha de betabel!"
 
 mensajeGerminacion="¡Tu cultivo de betabel por fin ha germinado!" 
-+"Las plantas de betabel necesitan al menos 6 horas de luz solar directa al día." 
+"Las plantas de betabel necesitan al menos 6 horas de luz solar directa al día." 
 
 mensajeTrasplante="¡Tu cultivo de betabel está listo para trasplantarse!"
-+"Verifica que la plantita tiene entre 10 y 15 cm de longitud, o de 3 a 4 hojas"
+"Verifica que la plantita tiene entre 10 y 15 cm de longitud, o de 3 a 4 hojas"
 mensajeCosecha="¡Tu cultivo de betabel esta listo para cosecharse!"
-+"Cosechar cuando el diámetro del bulbo sea de 8 a 10 cm y las hojas estén amarillas."
+"Cosechar cuando el diámetro del bulbo sea de 8 a 10 cm y las hojas estén amarillas."
